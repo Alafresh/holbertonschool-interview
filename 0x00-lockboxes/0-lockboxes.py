28caa28328b92a368a28c0e11dc1a515e1198d9c
Alafresh <868@holbertonschool.com>
2020-04-21 15:17:37 -0500

    Fix pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-"""Algorithm  that unlock all boxes"""
+    """Algorithm  that unlock all boxes"""
 
     keySet = []
 
-"""Check box by box"""
+    """Check box by box"""
     for i in range(len(boxes)):
         for j in boxes[i]:
             if j >= len(boxes):
                 return False
             keySet.append(j)
 
-"""Hardcode"""
+    """Hardcode"""
     if len(set(keySet)) == len(boxes):
         return True
     else:
