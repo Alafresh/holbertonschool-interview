41441737125abb344e1ce363d9544b3a0f8fa46f
Alafresh <868@holbertonschool.com>
2020-04-21 15:48:04 -0500

    Lockboxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,17 +2,18 @@
 def canUnlockAll(boxes):
     """Algorithm  that unlock all boxes"""
 
-    keySet = []
+    keySet = [0]
 
     """Check box by box"""
-    for i in range(len(boxes)):
+    for i in keySet:
         for j in boxes[i]:
             if j >= len(boxes):
                 return False
-            keySet.append(j)
+            elif j < len(boxes) and j not in keySet:
+                keySet.append(j)
 
     """Hardcode"""
-    if len(set(keySet)) == len(boxes):
+    if len(keySet) == len(boxes):
         return True
     else:
         return False
