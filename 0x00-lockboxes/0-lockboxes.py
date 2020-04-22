b67fc152b1c02921936f02d20b9325b48572e7eb
Alafresh <868@holbertonschool.com>
2020-04-21 22:57:44 -0500

    Update the comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
-    """Algorithm  that unlock all boxes"""
 
+    """first Box"""
     keySet = [0]
 
-    """Check box by box"""
+    """looks for the key inside the first box and gets the keys"""
     for i in keySet:
         for j in boxes[i]:
             if j not in keySet and j < len(boxes):
                 keySet.append(j)
 
-    """Hardcode"""
+    """it returns true if get all the keys otherwise false"""
     if len(keySet) == len(boxes):
         return True
     else:
