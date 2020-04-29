b8d3ea4b44645034b900520046c98d30685bb03a
Alafresh <868@holbertonschool.com>
2020-04-28 20:45:24 -0500

    Insert in sorted linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -15,15 +15,21 @@ listint_t *insert_node(listint_t **head, int number)
         return (NULL);
     while (current != NULL)
     {
-        if (current->next->n < number)
-            current =current->next;
-        else
+        if (current->next->n > number)
         {
             node->n = number;
             node->next = current->next;
             current->next = node;
             return (node);
         }
+        if (current->next->next == NULL && current->next->n < number)
+        {
+            node->n = number;
+            node->next = NULL;
+            current->next->next = node;
+            return (node);
+        } 
+        current =current->next;
     }
 return (NULL);
 }
