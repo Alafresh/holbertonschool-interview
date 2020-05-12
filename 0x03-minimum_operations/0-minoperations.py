9a7a8316b74406ee981c9669286d8da69f7552c0
Alafresh <868@holbertonschool.com>
2020-05-12 14:42:50 -0500

    Update minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+
+    """That means n cannot be operated"""
+    if n < 2:
+        return 0
+
+    i = 2
+    h = 0
+    while i <= n:
+        if n % i == 0:
+            n /= i
+            h += i
+        else:
+            i += 1
+    return h
