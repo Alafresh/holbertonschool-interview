2066dca7ebdf2d0c5d4dbb9615750b19ca8719db
Alafresh <868@holbertonschool.com>
2020-08-05 21:46:41 -0500

    N queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""The N queens puzzle is the challenge of placing N non-attacking queens on an N×N """
+"""The N queens puzzle is the challenge
+of placing N non-attacking queens on an N×N """
+
 import sys
+
 
 def start(e):
     s = []
