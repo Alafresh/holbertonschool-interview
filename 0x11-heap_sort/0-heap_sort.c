af41d278e4fe0855a50c47ba24249173c6ff8e14
Alafresh <868@holbertonschool.com>
2020-09-02 23:42:56 -0500

    heap sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+
+/**
+ * heap_sort - sorts an array of integers in ascending order using the Heap sort algorithm
+ * @array: array of numbers
+ * @size: Size of the array
+ * Return: 0
+ */
+void heap_sort(int *array, size_t size)
+{
+	size_t len = size, raiz, tmp;
+	ssize_t ult, parent;
+	int buff;
+
+	if (size < 2)
+		return;
+	while (len > 1)
+	{
+		ult = size - 1;
+		parent = (ult - 1) / 2;
+		while (parent >= 0)
+		{
+			parent--;
+			raiz = parent;
+			while ((2 * raiz + 1) < len)
+			{
+				tmp = raiz;
+				if (array[(2 * raiz + 1)] > array[raiz])
+					tmp = (2 * raiz + 1);
+				if (2 * raiz + 2 < len && array[2 * raiz + 2] > array[tmp])
+					tmp = 2 * raiz + 2;
+				if (tmp == raiz)
+					break;
+				buff = array[raiz];
+				array[raiz] = array[tmp];
+				array[tmp] = buff;
+				print_array(array, size);
+				raiz = tmp;
+			}
+		}
+		buff = array[0];
+		array[0] = array[len - 1];
+		array[len - 1] = buff;
+		print_array(array, size);
+		len--;
+	}
+}
